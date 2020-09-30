0e371b880063aa80bbe6b65960ed9e6242f064fa
cemonel <cemonel97@gmail.com>
2020-09-30 15:33:15 +0300

    Add queues migrations

--- mubasir/queues/models.py
+++ mubasir/queues/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.core.validators import RegexValidator
 
 from hipo_django_core.models import AbstractBaseModel
 
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+
 
 class Queue(AbstractBaseModel):
-    name = models.CharField()
+    name = models.CharField(max_length=32, validators=[alphanumeric])
     items = JSONField()
+
     channel = models.ForeignKey("slack.SlackChannel", related_name="queues", on_delete=models.CASCADE)
 
-    created_by = models.CharField()
+    created_by = models.CharField(max_length=155)
 
     def __str__(self):
         return f"{self.name}"
