a47c0d3cab97689c0bfd4762196040acb9c80f91
cemonel <cemonel97@gmail.com>
2020-09-30 15:25:01 +0300

    Remove redundant class

--- mubasir/queues/models.py
+++ mubasir/queues/models.py
@@ -13,23 +13,3 @@ class Queue(AbstractBaseModel):
 
     def __str__(self):
         return f"{self.name}"
-
-
-class SlackWorkspace(models.Model):
-    access_token = models.CharField(max_length=255)
-    team_name = models.CharField(max_length=255)
-    team_id = models.CharField(max_length=255, unique=True)
-    team_domain = models.CharField(max_length=255, blank=True)
-
-    class Meta:
-        verbose_name = "Slack Workspace"
-        verbose_name_plural = "Slack Workspaces"
-
-    def __str__(self):
-        return f"{self.team_name}"
-
-    def update_team_info(self):
-        """
-        Update team information
-        """
-        client = slack.WebClient(token=self.access_token)
