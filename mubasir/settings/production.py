6e94e343a68b523c5ccb5a368dbb3b12e20438cb
Efe Öge <efeoge@gmail.com>
2020-10-02 12:01:25 +0300

    Fix static

--- mubasir/settings/production.py
+++ mubasir/settings/production.py
@@ -30,3 +30,15 @@ sentry_sdk.init(
     dsn=secrets.SENTRY_DSN,
     integrations=[DjangoIntegration()]
 )
+
+# django-storages
+#
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_OVERWRITE = False
+AWS_STORAGE_BUCKET_NAME = "mubasir"
+ROOT_MEDIA_URL = 'https://{}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
+MEDIA_URL = ROOT_MEDIA_URL + 'media/'
+STATIC_URL = ROOT_MEDIA_URL + 'static/'
+AWS_ACCESS_KEY_ID = secrets.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = secrets.AWS_SECRET_ACCESS_KEY
